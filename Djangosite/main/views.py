d3c97128d4bf5540bd914ecaa7116a4e9a069775
Grigoriy Trofimov <grigorii_trofimov@mail.ru>
2024-05-10 15:45:30 +0300

    updating index.html adding data from database

--- Djangosite/main/views.py
+++ Djangosite/main/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse , HttpResponseNotFound
 from .models import Person
+from .models import Skills
+from .models import Education
+from .models import Experience
+import datetime
 
 def index(request):
     person =  Person.objects.all()
-    return render(request, 'main/index.html',{'title': 'My site', 'Person': person[0]})
+    skills = Skills.objects.all()
+    education = Education.objects.all()
+    experience = Experience.objects.all()
+
+    return render(request, 'main/index.html',{'title': 'My site', 'Person': person[0], 'Skills': skills, 'Education':education, 'Experience' :experience})
 
 def projects(request):
     return render(request, 'main/projects.html')
