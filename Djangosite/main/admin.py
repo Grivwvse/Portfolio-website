52f7eff82b2808cc113e3225a40de51ee4f51528
Grigoriy Trofimov <grigorii_trofimov@mail.ru>
2024-05-10 20:21:10 +0300

    configuring the django admin panel

--- Djangosite/main/admin.py
+++ Djangosite/main/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'fullName', 'about', 'post', 'photo')
+    list_display_links=('id', 'fullName')
+    search_fields = ('fullName', 'about')
+
+class ProjectsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'projName', 'description', 'link', 'preson')
+    list_display_links=('id', 'projName')
+    search_fields = ('projName', 'description')
+
+class SkillsAdmin(admin.ModelAdmin):
+    list_display = ('id','skiName', 'preson')
+    list_display_links=('id', 'skiName')
+    search_fields = ('id', 'skiName')
+
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'comName', 'post', 'dateStart', 'dateEnd','preson')
+    list_display_links=('id', 'comName')
+    search_fields = ('comName', 'post')
+
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'eduName', 'eduDepartment', 'eduQualification', 'dateStart', 'dateEnd','preson')
+    list_display_links=('id', 'eduName')
+    search_fields = ('eduName', 'eduDepartment')
+
+admin.site.register(Projects, ProjectsAdmin)
+admin.site.register(Skills, SkillsAdmin)
+admin.site.register(Experience, ExperienceAdmin)
+admin.site.register(Education, EducationAdmin)
+admin.site.register(Person, PersonAdmin)
--- Djangosite/main/models.py
+++ Djangosite/main/models.py
@@ -2,35 +2,56 @@ from django.db import models
 from django.urls import reverse
 
 class Person(models.Model):
-    fullName = models.CharField(max_length=255)
-    about = models.TextField()
-    post = models.CharField(max_length=255)
-    photo = models.ImageField(upload_to="photos/")
+    fullName = models.CharField(max_length=255, verbose_name="ФИО")
+    about = models.TextField(verbose_name="Обо мне")
+    post = models.CharField(max_length=255, verbose_name="Должность")
+    photo = models.ImageField(upload_to="photos/", verbose_name="Фото")
+
+    class Meta:
+        verbose_name = 'Вы'
+        verbose_name_plural = 'Вы'
 
 class Projects(models.Model):
     preson = models.ForeignKey(Person,on_delete=models.CASCADE)
-    projName = models.CharField(max_length=255)
-    description = models.TextField()
-    link = models.TextField()
+    projName = models.CharField(max_length=255, verbose_name="Имя проекта")
+    description = models.TextField(verbose_name="Описание проекта")
+    link = models.TextField(verbose_name="Ссылка")
 
     def get_absolute_url(self):
         return reverse('project', kwargs={'project_id': self.pk})
+    
+    class Meta:
+        verbose_name = 'Проекты'
+        verbose_name_plural = 'Проекты'
 
 class Skills(models.Model):
     preson = models.ForeignKey(Person,on_delete=models.CASCADE)
-    skiName = models.TextField()
+    skiName = models.TextField(verbose_name="Наименование навыка")
+
+    class Meta:
+        verbose_name = 'Навыки'
+        verbose_name_plural = 'Навыки'
 
 class Experience(models.Model):
     preson = models.ForeignKey(Person,on_delete=models.CASCADE)
-    comName = models.TextField()
-    post = models.CharField(max_length=255)
-    dateStart = models.DateField()
-    dateEnd = models.DateField()
+    comName = models.TextField(verbose_name="Компания")
+    post = models.CharField(max_length=255, verbose_name="Должность")
+    dateStart = models.DateField(verbose_name="Начало")
+    dateEnd = models.DateField(verbose_name="Конец")
+
+    class Meta:
+        verbose_name = 'Опыт работы'
+        verbose_name_plural = 'Опыт работы'
+
 
 class Education(models.Model):
     preson = models.ForeignKey(Person,on_delete=models.CASCADE)
-    eduName = models.TextField()
-    eduDepartment = models.TextField(default='IT')
-    eduQualification = models.TextField(default='IT')
-    dateStart = models.DateField()
-    dateEnd = models.DateField()
+    eduName = models.TextField(verbose_name="Учебн. учреждение")
+    eduDepartment = models.TextField(default='IT', verbose_name="Факультет")
+    eduQualification = models.TextField(default='IT', verbose_name="Квалификация")
+    dateStart = models.DateField(verbose_name="Начало")
+    dateEnd = models.DateField(verbose_name="Конец")
+
+    class Meta:
+        verbose_name = 'Образование'
+        verbose_name_plural = 'Образование'
